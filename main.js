2e8624fd88378ea7c22f880a8e5b27e3eccdcb13
Eric Masi <eric@gitmasi.com>
2019-07-07 11:23:31 -0400

    add open/close animation, add styles

---
+++ main.js
@@ -0,0 +1,27 @@
+const clickHeadings = document.querySelectorAll('.clickable-heading');
+
+document.addEventListener('DOMContentLoaded', () => {
+  clickHeadings[0].nextElementSibling.style.display = 'block';
+  clickHeadings[0].nextElementSibling.classList.add('open');
+});
+
+clickHeadings.forEach(el => {
+  el.addEventListener('click', toggleOpenClose);
+});
+
+function toggleOpenClose(e) {
+  const collapsible = e.target.nextElementSibling;
+  const styles = getComputedStyle(collapsible);
+  const time = styles.getPropertyValue('--time').replace('ms', '');
+
+  if (collapsible.classList.contains('open')) {
+    setTimeout(() => {
+      collapsible.style.display = 'none';
+    }, time);
+    collapsible.classList.remove('open');
+  } else {
+    collapsible.style.display = 'block';
+    collapsible.classList.add('open');
+  }
+
+}

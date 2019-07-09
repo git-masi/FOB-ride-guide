42ff387c1a2391cc68deaac603145a5d51ec19b7
Eric Masi <eric@gitmasi.com>
2019-07-09 08:11:47 -0400

    add resize event listener

--- main.js
+++ main.js
@@ -15,3 +15,24 @@ if (window.innerWidth >= 600) {
     }
   }
 }
+
+// doesn't trigger resize when using windows hot keys
+window.addEventListener('resize', () => {
+  if (window.innerWidth >= 600) {
+    const clickHeadings = document.querySelectorAll('.clickable-heading');
+    clickHeadings.forEach(el => {
+      el.addEventListener('click', toggleOpenClose);
+    });
+  
+    function toggleOpenClose(e) {
+      const parent = e.target.parentElement;
+      const content = e.target.nextElementSibling;
+  
+      if (parent.style.maxHeight == '') {
+        parent.style.maxHeight = content.scrollHeight + 'px';
+      } else {
+        parent.style.maxHeight = '';
+      }
+    }
+  }
+});

95b294215b8d1c4e6ca02839d2721a4690904289
Eric Masi <eric@gitmasi.com>
2019-07-09 07:56:21 -0400

    don't run script on mobile

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-const clickHeadings = document.querySelectorAll('.clickable-heading');
+if (window.innerWidth >= 600) {
+  const clickHeadings = document.querySelectorAll('.clickable-heading');
+  clickHeadings.forEach(el => {
+    el.addEventListener('click', toggleOpenClose);
+  });
 
-clickHeadings.forEach(el => {
-  el.addEventListener('click', toggleOpenClose);
-});
+  function toggleOpenClose(e) {
+    const parent = e.target.parentElement;
+    const content = e.target.nextElementSibling;
 
-function toggleOpenClose(e) {
-  const parent = e.target.parentElement;
-  const content = e.target.nextElementSibling;
-
-  if (parent.style.maxHeight == '') {
-    parent.style.maxHeight = content.scrollHeight + 'px';
-  } else {
-    parent.style.maxHeight = '';
+    if (parent.style.maxHeight == '') {
+      parent.style.maxHeight = content.scrollHeight + 'px';
+    } else {
+      parent.style.maxHeight = '';
+    }
   }
 }
